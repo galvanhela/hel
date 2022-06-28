0f9913c8d8813b861234767b1140da7d5ea4b919
galvanhela <88916308+galvanhela@users.noreply.github.com>
2022-06-28 18:59:32 -0500

    Add files via upload
    uploading a new file

---
+++ 14looping.py
@@ -0,0 +1,12 @@
+pizzas = ['three cheeses', 'hawaian', 'pepperoni']
+for pizza in pizzas:
+	print (f' the {pizza.title ()} pizza is the best')
+print ("I like pizza a lot, but i couldn't eat them everyday because i would die.\n")
+
+animals = ['dolphin', 'shark', 'killer whale']
+for animal in animals:
+	print (f"{animal.title()}")
+	print (f" The {animal.title()} is really gorgeous")
+	print (f" is amazig its swimming")
+print ('I like these three animals')
+	
